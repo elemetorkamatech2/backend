3badc10808f7fe4f7de4ef4cce9ba4bccc6212d2
chayaRivkaErenfeld <erenfeld226@gmail.com>
2023-05-22 16:39:00 +0300

    createProject

---
+++ User.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(id, name, mail,password ){
+        this.id=id;
+        this.name-name;
+        this.mail=mail;
+        this.password= password;
+    }
+}
+module.exports=User;
---
+++ controller/UserController.js
@@ -0,0 +1,13 @@
+const User = require('../User')
+
+var listUsres = [
+    { id: 1, name: "lea", mail: "fff@ddd", password: "999999" },
+    { id: 2, name: "Yehudit", mail: "fff@ddd", password: "999999" },
+    { id: 3, name: "Rivky", mail: "fff@ddd", password: "055332545" },
+]
+
+module.exports = {
+    getAll: async (req, res) => {
+        res.send(listUsres)
+    }
+}
