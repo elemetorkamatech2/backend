e13635790253d6816d894fd0526ac289a598e421
BrainiTenenboim <133571943+BrainiTenenboim@users.noreply.github.com>
2023-05-24 23:28:25 +0300

    Merge branch 'main' of https://github.com/elemetorkamatech2/backend

--- useLogger.js
+++
@@ -1,5 +0,0 @@
-// const logger = require('./logger');
-// logger.debug('This is a debug message');
-// logger.info('This is an info message');
-// logger.warn('This is a warning message');
-// logger.error('This is an error message');
