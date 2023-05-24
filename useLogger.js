4665e01c1799975a65f251d3798cb5ce9d7294ac
BrainiTenenboim <133571943+BrainiTenenboim@users.noreply.github.com>
2023-05-24 22:48:25 +0300

    Create useLogger.js

---
+++ useLogger.js
@@ -0,0 +1,5 @@
+// const logger = require('./logger');
+// logger.debug('This is a debug message');
+// logger.info('This is an info message');
+// logger.warn('This is a warning message');
+// logger.error('This is an error message');
