929870756706db873fb0bf00798369133b113c6a
YeuditS <ysad208@gmail.com>
2023-05-31 13:33:05 +0300

    y

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express=require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const app = express()
-const port=3002
+const port=3009
 
 //הרשאות  
 app.use((req, res, next) => {
@@ -28,12 +30,35 @@ const UserR = require('./routes/UserRoute')
 app.use('/UserRoute',UserR)
 
 //ניתוב ברירת מחדל שיכתוב הערה
-app.get('/', (req, res) => {
-    res.status(200).json({ 'message': 'hello!!!!! :)' })
-})
+// app.get('/', (req, res) => {
+//     res.status(200).json({ 'message': 'hello!!!!! :)' })
+// })
+app.get('/api/getAll', (req, res) => {
+    res.json(listUsres);
+  });
+const options = {
+    swaggerDefinition: {
+      swagger: '2.0',
+      info: {
+        title: 'My API',
+        version: '1.0.0',
+        description: 'API for my application'
+      },
+      basePath: '/api',
+      schemes: ['http'],
+    },
+    apis: ['./routes/.js'],
+  };
+  
+  const specs = swaggerJsdoc(options);
+  
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 //שיביא בקונסול את כתובת השרת
 app.listen(port, () => {
     console.log('wow');
     console.log(`my app is listening on http://localhost:${port}`);
 
 })
+
+
+
--- controller/UserController.js
+++ controller/UserController.js
@@ -4,6 +4,7 @@ var listUsres = [
     { id: 1, name: "lea", mail: "fff@ddd", password: "999999" },
     { id: 2, name: "Yehudit", mail: "fff@ddd", password: "999999" },
     { id: 3, name: "Rivky", mail: "fff@ddd", password: "055332545" },
+    { id: 4, name: "Rivky", mail: "fff@ddd", password: "055332545" },
 ]
 
 module.exports = {
